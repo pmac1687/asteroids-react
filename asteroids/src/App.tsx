7708295987b001755c597214958ffd5bcbc1184b
pmac1687 <pmac1687@outlook.com>
2021-01-14 13:53:21 -0800

    random enemy

--- asteroids/src/App.tsx
+++ asteroids/src/App.tsx
@@ -1,20 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
 import React, { useState } from 'react';
-import { ReactElement } from 'react';
 ///import ReactDOM from 'react';
 import './App.css';
 import GameMenu from './GameMenu';
 import Ship from './Ship';
 import Enemy from './Enemy';
+import { ReactElement } from 'react';
 ///import GameMenu from './App';
 
 function App() {
     ///come in and pass random properties for speen andirection and spawn blah when instantiating
     const [show, setShow] = useState(true);
-    const [enenmyCount, setEnemyCount] = useState(3);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const [enemyArray, setEnemyArray] = useState<Array<ReactElement>>();
+    const [enemyCount, setEnemyCount] = useState(4);
+    const [enemyArray, setEnemyArray] = useState<Array<ReactElement>>([]);
     ///const hello = ['helllo', 'goodbye', 'bye'];
     function onClick() {
         setShow(false);
@@ -23,18 +22,42 @@ function App() {
         if (shipAdj) {
             shipAdj.style.display = 'none';
         }
-        startGameLoop();
+        populateEnenmies();
     }
-    function startGameLoop() {
-        console.log(<div>hello</div>);
-        const enemies = [];
-        for (let x = 0; x < enenmyCount; x++) {
-            const enemy = <Enemy key={`${x}`} />;
-            enemies.push(enemy);
-        }
-        setEnemyArray(enemies); ///[<div key={1}>hello</div>, <div key={2}>goodbye</div>]);
-        console.log(enemyArray);
-        setEnemyCount(4);
+    function populateEnenmies() {
+        const arr = enemyArray;
+        const leftCoord = Math.floor(Math.random() * 100 + 1);
+        const rightCoord = Math.floor(Math.random() * 100 + 1);
+        console.log(rightCoord);
+        console.log('left', leftCoord);
+        console.log(window.innerWidth);
+        const leftSideEnemy = (
+            <div title="-4" style={{ backgroundColor: 'blue', left: 1820, top: 630 }} key={enemyCount - 1} id="enemy">
+                hello
+            </div>
+        );
+        const rightSideEnemy = (
+            <div style={{ backgroundColor: 'green' }} key={enemyCount} id="enemy">
+                hello
+            </div>
+        );
+        console.log(leftSideEnemy);
+        enemyArray?.push(leftSideEnemy);
+        enemyArray?.push(rightSideEnemy);
+        console.log(leftSideEnemy);
+        /*for (let x = 0; x < 2; x++) {
+            const enemy = (
+                <div style={{ backgroundColor: 'blue' }} key={x} id="enemy">
+                    hello
+                </div>
+            );
+            if (arr) {
+                arr.push(enemy);
+            }
+        }*/
+        console.log(arr);
+        setEnemyCount(2);
+        setEnemyArray(arr);
     }
     return (
         <div id="App" className="App">
@@ -45,6 +68,7 @@ function App() {
                     return <div key={index}>{item}</div>;
                 })}
             </div>
+            <Enemy />
         </div>
     );
 }
